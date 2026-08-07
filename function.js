687819b216bef242ad56b3dba63a35c125a8740f
unknown <utshobjaman2012@gmail.com>
2026-08-07 21:16:31 +0600

    default parameter and skiping values func.js

--- function.js
+++ function.js
@@ -1,18 +1,24 @@
 "use strict";
 
-//const bookings = [];
+const bookings = []; // sotring the objcet inside the array  
 
-const createBooking  =  function(numPassenger,flightNum,price){
+const createBooking  =  function(numPassenger = 1 ,flightNum = "HH1" ,price =199 * numPassenger){
+    // ES 5 ugley boiler plate 
+    // numPassenger = numPassenger || 1;
+    // price = price || 199;
     const booking = {
         numPassenger,
         flightNum,
         price,
     };
     console.log(booking);
-    //bookings.push(booking);
+    bookings.push(booking);
 
 };
 
-createBooking("5","LH11");
-
+createBooking("5","LH11",2500);
+createBooking(undefined,'B85L');
+createBooking(5,undefined,599);
+createBooking(78,"busR099",1999);
+createBooking(10,"t100");
 
