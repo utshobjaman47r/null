2c0bb0ba6ac34dfbdc77df45406b863d63cc9cf4
unknown <utshobjaman2012@gmail.com>
2026-08-07 19:50:51 +0600

    making a bookings function in functon.js

---
+++ function.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//const bookings = [];
+
+const createBooking  =  function(numPassenger,flightNum,price){
+    const booking = {
+        numPassenger,
+        flightNum,
+        price,
+    };
+    console.log(booking);
+    //bookings.push(booking);
+
+};
+
+createBooking("5","LH11");
+
+
